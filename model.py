da598c76eb4a1db49ea808d04d72ad0aa658ede0
suman <suman@macmac.local>
2015-10-29 12:36:58 -0700

    First commit

---
+++ model.py
@@ -0,0 +1,144 @@
+"""Models and database functions for Project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """User of Nucleus/Family Network/Shared list."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name=db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    mobile = db.Column(db.Integer, nullable=False)
+    user_location_name = db.Column(db.Integer, nullable = True)
+    user_location_address = db.Column(db.String(100), nullable = True)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "User ID: {}, Name: {}, email:{}, password: {}, mobile#, User location: {}, User Address: {}: {}".format(self.user_id,
+                                     self.name, self.email, self.password, self.mobile,
+                                     self.location_name, self.location_address)
+
+
+class List(db.Model):
+    """The list."""
+
+    __tablename__ = "lists"
+
+    list_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    list_type = db.Column(db.String(20))
+    due_date = db.Column(db.DateTime, nullable=True)
+    list_location_name = db.Column(db.String(50), nullable=True)
+    list_location_address = db.Column(db.String(100), nullable=True)
+    created_by = db.Column(db.String(50), nullable = False)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "List ID: {}, List name: {}, List Type:{}, due_date: {}, list location: {}, list Address: {}, Created by: {}".format(self.list_id,
+                                     self.name, self.list_type, self.due_date,
+                                     self.location_name,self.location_address, self.created_by)
+
+
+class Groups(db.Model):
+    """Tying users and lists"""
+
+    __tablename__ = "groups"
+
+    group_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.user_id'))
+    list_id = db.Column(db.Integer,db.ForeignKey('lists.list_id'))
+    permission = db.Column(db.Boolean, nullable = False, default = True)
+
+    # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("groups"))
+
+    # Define relationship to list
+    checklist = db.relationship("List",
+                            backref=db.backref("groups"))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "Group ID: {}, User ID: {}, List ID: {}".format(self.group_id,
+                                     self.user_id, self.list_id)
+
+
+
+
+class To_Do(db.Model):
+    """To-Do list list type"""
+
+    __tablename__ = "to_dos"
+
+    to_do_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    list_id = db.Column(db.Integer, db.ForeignKey('lists.list_id'))
+    item = db.Column(db.String(64), nullable=True)
+    due_date = db.Column(db.DateTime, nullable=True)
+    status_notdone = db.Column(db.Boolean, nullable=False, default=True)
+    todo_location_name = db.Column(db.String, nullable=True)
+    todo_location_address = db.Column(db.String, nullable=True)
+
+    #Define relationship to lists
+    lists = db.relationship("Lists",
+                                backref=db.backref("to_dos"))
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "List_Items ID:{}, List ID: {}, Item: {}, Due Date: {}, Status(ongoing?): {}".format(self.list_items_id,
+                                     self.list_id, self.item, self.due_date, self.status_notdone,
+                                     self.todo_location_name, self.todo_location_address)
+
+
+
+class Shopping(db.Model):
+    """Shopping list list type"""
+
+    __tablename__ = "shoppings"
+
+    shopping_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    list_id = db.Column(db.Integer, db.ForeignKey('lists.list_id'))
+    item = db.Column(db.String(64), nullable=True)
+    due_date = db.Column(db.DateTime, nullable=True)
+    status_notdone = db.Column(db.Boolean, nullable=False, default=True)
+
+    #Define relationship to lists
+    lists = db.relationship("Lists",
+                                backref=db.backref("shoppings"))
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "List_Items ID:{}, List ID: {}, Item: {}, Due Date: {}, Status(ongoing?): {}".format(self.list_items_id,
+                                     self.list_id, self.item, self.due_date, self.status_notdone)
+
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our SQLite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+#    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # if we run this module interactively, able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ server.py
@@ -0,0 +1,82 @@
+"""Movie Ratings."""
+
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import connect_to_db, db
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails silently.
+# This is horrible. Fix this so that, instead, it raises an error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
+
+@app.route('/login', methods=['GET'])
+def login():
+    """login"""
+
+    return render_template("login.html")
+
+@app.route('/login', methods=['POST'])
+def login_info():
+    """login info"""
+
+    # Get form variables
+    email = request.form["email"]
+    password = request.form["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("User not found!")
+        return redirect("/login")
+
+    if user.password != password:
+        flash("Wrong password")
+        return redirect("/login")
+
+    session["user_id"] = user.user_id
+
+    flash("Logged in")
+ #   return redirect("/users/%s" % user.user_id)
+
+
+    return redirect('/')
+
+@app.route('/register', methods=['GET'])
+def register():
+    """register"""
+
+    return render_template("register.html")
+
+@app.route('/register', methods=['GET'])
+def register_info():
+    """register"""
+
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run()
